4ad013084f6429c31a2f168a3ef04681a82275d9
kokilgupta <kokilgupta682@gmail.com>
2020-05-30 13:23:18 +0530

    Updates

---
+++ precache-manifest.6a7a45fe5d777b921e26333c4a1e0b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a043d3c21b89f0990197ba2687793e66",
+    "url": "/magicalbutton/index.html"
+  },
+  {
+    "revision": "d3ef9d13d1d4eb8f771c",
+    "url": "/magicalbutton/static/css/main.81a8eef0.chunk.css"
+  },
+  {
+    "revision": "263ac98aa0ed90af1f06",
+    "url": "/magicalbutton/static/js/2.c7b23c08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magicalbutton/static/js/2.c7b23c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ef9d13d1d4eb8f771c",
+    "url": "/magicalbutton/static/js/main.d379b289.chunk.js"
+  },
+  {
+    "revision": "60dd28bac1fdb3fd70e2",
+    "url": "/magicalbutton/static/js/runtime-main.1b49444b.js"
+  }
+]);
